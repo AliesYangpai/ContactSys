4c8ad7daa2e57bf1c496ad14f8c8f4b367282e49
AliesYangpai <yangpai_beibei@163.com>
2021-01-13 23:11:54 +0800

    add search modify and clearAll and finish mission

--- ContactSys/main.cpp
+++ ContactSys/main.cpp
@@ -20,6 +20,10 @@ struct ContactsBook
 	short current_size = 0; // 当前的人数列表数量
 };
 
+void showText(const char* p_text)
+{
+	cout << p_text << endl;
+}
 
 // 添加联系人
 void addContact(ContactsBook * p_cb)
@@ -119,28 +123,143 @@ void delContact(ContactsBook * p_cb)
 	
 }
 
+/*
+ 显示单个信息
+*/
+void showSingleContact(const Person* p_person)
+{
+	cout << p_person->name << "\t";
+	cout << p_person->gender << "\t";
+	cout << p_person->age << "\t";
+	cout << p_person->phone << "\t";
+	cout << p_person->address << endl;
+}
+/*
+  此处就按照练习人姓名搜索
+*/
+void doSelectContact(ContactsBook * p_cb) 
+{
+	cout << " 请输入联系人完整姓名：" << endl;
+	string name;
+	cin >> name;
+  int index =isExist(p_cb,name);
+	if (index != -1)
+	{
+		cout << " 查询结果如下：" << endl;
+		showSingleContact(&(p_cb->personArr[index]));
+	}
+	else
+	{
+		cout << " 无此联系人" << endl;
+	}
+	system("pause");
+	system("cls");
+
+}
 
 void showAllContacts(ContactsBook* p_cb)
 {
 	if (p_cb->current_size == 0)
 	{
 		cout << " 暂无联系人" << endl;
+		system("pause");
+		system("cls");
 		return;
 	}
-
+	cout << " 查询结果如下：" << endl;
 	for (int i = 0; i < p_cb->current_size; i++)
 	{
 	 Person person =p_cb->personArr[i];
-	// cout << person.name << " " << person.gender << " " << person.age << " " << person.phone << " " << person.address << endl;
-	 cout << person.name << "\t";
-	 cout << person.gender << "\t";
-	 cout << person.age << "\t";
-	 cout << person.phone << "\t";
-	 cout << person.address << endl;
+	 showSingleContact(&person);
 	}
 	system("pause");
 	system("cls");
 
+}
+
+
+void doModifyContact(ContactsBook* p_cb)  
+
+{
+	cout << " 请输入要修改的联系人姓名：" << endl;
+	string name;
+	cin >> name;
+	int	index = isExist(p_cb, name);
+
+	if (index != -1)
+	{
+		cout << " 要修改的人员信息如下：" << endl;
+		showSingleContact(&(p_cb->personArr[index]));
+		
+		cout << " 请输入新姓名：" << endl;
+		string newName;
+		cin >> newName;
+		p_cb->personArr[index].name = newName;
+
+		cout << " 请输入新性别" << endl;
+		while (true)
+		{
+			cout << "1--男" << endl;
+			cout << "2--女" << endl;
+			short newGender;
+			cin >> newGender;
+			if (newGender == 1 || newGender == 2)
+			{
+				p_cb->personArr[index].gender = newGender == 1 ? "男" : "女";
+				break;
+			}
+			cout << " 输入不合法，请重新输入：" << endl;
+		}
+
+		cout << " 请输入新年龄：" << endl;
+		short newAge;
+		cin >> newAge;
+		p_cb->personArr[index].age = newAge;
+
+
+		cout << " 请输入新的电话号码：" << endl;
+		string newNum;
+		cin >> newNum;
+		p_cb->personArr[index].phone = newNum;
+
+		cout << " 请输入新的地址：" << endl;
+		string newAddress;
+		cin >> newAddress;
+		p_cb->personArr[index].address = newAddress;
+
+		cout << "~~更新完成~~" << endl;
+
+		system("pause");
+		system("cls");
+	}
+	else
+	{
+		cout << "查无此人" << endl;
+	}
+	system("pause");
+	system("cls");
+}
+
+
+void doClearAllContacts(ContactsBook * p_cb) 
+{
+	cout << " 确认清空联系人吗？（Y/N）" << endl;
+	char enter;
+	cin >> enter;
+	if (enter == 'Y')
+	{
+		for (int i = 0; i < p_cb->current_size; i++)
+		{
+			//p_cb->personArr[i] = { 0 };
+			memset(&(p_cb->personArr[i]),0,sizeof(p_cb->personArr[i]));
+		}
+	
+		p_cb->current_size = 0;
+		cout << "联系人已清空" << endl;
+	}
+
+	system("pause");
+	system("cls");
 }
 
 void showMenu()  {
@@ -175,10 +294,13 @@ void showSys(ContactsBook* p_cb)
 			delContact(p_cb);
 			break;
 		case 4:  // 4、查找联系人
+			doSelectContact(p_cb);
 			break;
 		case 5:  // 5、修改联系人
+			doModifyContact(p_cb);
 			break;
 		case 6:  // 6、清空联系人
+			doClearAllContacts(p_cb);
 			break;
 		case 0:  // 0、退出通讯录
 			cout << "再见" << endl;
